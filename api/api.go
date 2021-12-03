7b5dab88d1c27965c61bba708c532cb19e15c4ac
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-12-03 11:30:17 +0900

    Split package for api

---
+++ api/api.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var count int
+
+func HandlerMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[HandlerMain] Start!")
+	count++
+	fmt.Fprintf(w, "Hello!!! You have visited %d times !", count)
+	fmt.Println("[HandlerMain] Finish!")
+}
--- main.go
+++ main.go
@@ -3,18 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/kunikuni03/simple-web-api/api"
 )
 
-var count int
-
 func main() {
-	http.HandleFunc("/", apiHandler)
+	http.HandleFunc("/api", api.HandlerMain)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Server error. process exit.")
 	}
-}
-
-func apiHandler(w http.ResponseWriter, r *http.Request) {
-	count++
-	fmt.Fprintf(w, "Hello!!! You have visited %d times !", count)
 }
